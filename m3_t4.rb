fee4ae8dd915962305a67f4d317503f15861d0a8
Andr7734 <andrew26081346@gmail.com>
2022-11-19 03:26:08 +0200

    new

--- m3_t4.rb
+++ m3_t4.rb
@@ -1,6 +1,4 @@
-def task_4(a, b)
-    a = gets.strip.to_i
-    b = gets.strip.to_i
-    result = (a**b)
-    puts result
-    end
+a = gets.strip.to_i
+b = gets.strip.to_i
+result = (a <=> b)
+puts result
--- second.rb
+++ second.rb
@@ -1,1 +1,4 @@
-puts ('Another new file created')
+first_person_name = ('Andrii')
+second_person_name = ('Petro')
+puts first_person_name <=> second_person_name
+puts 44 && 77 ? ('Andrii') : ('Petro')
