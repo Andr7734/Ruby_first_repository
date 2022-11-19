c63b19599f4c3f45e7a8e892b41d263f77b4c5d8
Andr7734 <andrew26081346@gmail.com>
2022-11-19 05:58:30 +0200

    with '#'

--- m3_t4.rb
+++ m3_t4.rb
@@ -1,22 +1,26 @@
+# first interpretation
 x = gets.strip.to_i
 y = gets.strip.to_i
 puts result = (x <=> y)
 
+# second interpretation
 x = gets.strip.to_i
 y = gets.strip.to_i
 (x == y) ? puts(result = 0) : (x < y) ? puts(result = -1) : puts(result = 1)
 
+# third interpretation
 x = gets.strip.to_i
 y = gets.strip.to_i
 puts result = (x == y) ? 0 : (x < y) ? -1 : 1
 
+# fourth interpretation
 def res(x, y)
-    x = gets.strip.to_i
-    y = gets.strip.to_i
+    x, y = gets.strip.to_i, gets.strip.to_i
     result = (x <=> y)
   end
 puts res(x, y)
 
+# fifth interpretation
 def res(x, y)
     result = (x <=> y)
   end
