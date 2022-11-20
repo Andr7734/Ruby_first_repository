9fd931d2faae90eec2e6e35570af993f08ef4e2f
Andr7734 <andrew26081346@gmail.com>
2022-11-20 19:24:43 +0200

    cauchemar

--- second.rb
+++ second.rb
@@ -1,4 +1,9 @@
 first_person_name = ('Andrii')
 second_person_name = ('Petro')
 puts first_person_name <=> second_person_name
-puts 44 && 77 ? ('Andrii') : ('Petro')
+
+w = gets.strip.to_i <=> gets.strip.to_i
+puts (w == 0) ? ('Try again') : (w < 0) ? first_person_name : second_person_name
+
+w = gets.strip.to_i <=> gets.strip.to_i
+puts w
