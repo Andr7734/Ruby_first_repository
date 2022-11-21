ff11615ac56fafd72d98523bd0ad3fa1ab97951d
Andr7734 <andrew26081346@gmail.com>
2022-11-21 13:12:28 +0200

    ++

--- m4_t4.rb
+++ m4_t4.rb
@@ -3,7 +3,21 @@ def rgb(str)
         when ('red') then puts str
         when ('green') then puts str
         when ('blue') then puts str
-        else puts ('Я не знаю такого кольору')
-    end
-end
+        else puts ('ray')
+      end
+  end
 puts rgb(gets.strip)
+
+def rgb(str)
+    if str == 'red' || str == 'blue' || str == 'green'; puts str
+    else puts('ray') end
+  end
+  puts rgb(gets.strip)
+
+=begin
+  def rgb(str)
+    puts str = (('red') || ('blue') || ('green'))
+    else puts('ray') end
+  end
+  puts rgb(gets.strip)
+  =end
