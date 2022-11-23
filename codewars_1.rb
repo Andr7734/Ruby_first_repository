8bccb8f13a326f81656590e48b61c0c2d05e09c9
Andr7734 <andrew26081346@gmail.com>
2022-11-23 03:56:41 +0200

    delete first

--- codewars_1.rb
+++ codewars_1.rb
@@ -1,12 +1,3 @@
-=begin
-def mission(distance)
-    fuel = 2
-    gal_per_mile = 25
-    (fuel * gal_per_mile) >= distance
-  end
-puts mission(50)
-=end
-
 w, q, e = gets.strip.to_i, gets.strip.to_i, gets.strip.to_i
 def zero_fuel(distance, mpg, fuel_left)
     distance <= (mpg * fuel_left)
