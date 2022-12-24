0e9f40962ab7f1ccbbb5aadd1e93087f6de375b9
Andr7734 <andrew26081346@gmail.com>
2022-12-24 02:28:29 +0200

    new

---
+++ x.rb
@@ -0,0 +1,20 @@
+str = "Hello World!"
+str.downcase!()
+
+str1 = "World!"
+str2 = "Hello #{str1}"
+
+re = /[A-Z]/
+re.match?("Some – World")
+
+re = /A\-Z/
+re.match?("Some World")
+
+re = /[A\-Z]/
+re.match?("Some – World")
+
+# Find the word 'like'
+"Do you like cats?" =~ /like/
+
+str = 22022022
+puts palindrome?(22022022)
